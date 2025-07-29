6cbc7d644038072b809d5fcf8742ccfb5b702357
K4kug3n <maxime.rambosson@gmail.com>
2025-07-30 00:43:56 +0200

    Add tmux config for nvim colorscheme

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,7 @@ DOTFILES_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 declare -A SYMLINKS=(
     [".bash_aliases"]=".bash_aliases"
+    [".tmux.conf"]=".tmux.conf"
 
     ["nvim"]=".config/nvim" 
 )
