a845dddc5a9f79ff1ccdec36e674d17fa10e5456
Maxime R <maxime.rambosson@gmail.com>
2026-02-28 19:50:09 +0100

    Fix non-perfect nvim tab configuration

--- nvim/lua/user/options.lua
+++ nvim/lua/user/options.lua
@@ -57,7 +57,9 @@ vim.o.splitbelow = true
 
 -- Tab Size
 vim.o.tabstop = 4
+vim.o.softtabstop = 4
 vim.o.shiftwidth = 4
+vim.o.expandtab = true
 
 -- Sets how neovim will display certain whitespace characters in the editor.
 --  See `:help 'list'`
